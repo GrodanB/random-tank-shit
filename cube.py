41219691416787ea3e4126e42a8a3bbb0ef8017d
FegaMega <david.smidebrant@gmail.com>
2024-03-28 10:00:49 +0100

    tanks done
    need to add:
    shooting
    collision
    level

--- cube.py
+++ cube.py
@@ -1,17 +1,57 @@
-import pygame
+import pygame, math
 
-class Cube:
-    def __init__(self, x, y, width, height) -> None:
-        self.rect = pygame.rect.Rect(x, y, width, height)
+class Player:
+    def __init__(self, x, y, color) -> None:
+        self.rect = pygame.rect.Rect(x, y, 65, 50)
+        #needed because of shity rounding from rect.x being an int
+        self.pos: float = [x, y]
         self.rect.center = (self.rect.width/2, self.rect.height/2)
-        
         self.angle = 0
-        
-        self.color = (60, 0, 0)
-        self.orgimg = pygame.surface.Surface((self.rect.x, self.rect.y))
-        self.orgimg.fill(self.color)
+        self.oldAngle = self.angle
+        self.changeangle = 0
+        self.speed = 0
+        #Making tank
+        self.color = color
+        #main Surf
+        self.orgimg = pygame.surface.Surface((self.rect.width, self.rect.height)).convert_alpha()
+        #Barrel surf
+        self.barrel = pygame.surface.Surface((50, 10))
+        self.barrel.fill((self.color[0]*.75, self.color[1]*.75, self.color[2]*.75))
+        #Body Surf
+        self.body = pygame.surface.Surface((50, 50))
+        self.body.fill(self.color)
+
+        #filling surf
+        self.orgimg.blit(self.body, (0, 0))
+        self.orgimg.blit(self.barrel, (15, 20))
+
         self.img = pygame.transform.rotate(self.orgimg, self.angle)
         self.rect = self.img.get_rect()
+    def updateVal(self, speed=None, x=None, y=None, angle=None):
+        if angle != None:
+            self.changeangle += angle
+        if speed != None:
+            self.speed += speed
+        if x != None:
+            self.rect.x = x
+        if y != None:
+            self.rect.y = y
+    def update(self):
+        self.angle += self.changeangle
+        if self.angle != self.oldAngle:
+            size = self.rect.size
+            self.img = pygame.transform.rotate(self.orgimg, self.angle)
+            Topleft = [size[0]/2 + self.pos[0] - self.img.get_width()/2,  #X
+                       size[1]/2 + self.pos[1] - self.img.get_height()/2] #Y
+            self.pos = Topleft
+            self.rect = self.img.get_rect(topleft = Topleft)
+            self.oldAngle = self.angle
+        self.pos[0] += math.cos(math.radians(self.angle)) * self.speed
+        self.pos[1] -= math.sin(math.radians(self.angle)) * self.speed
 
-    def draw(self, screen):
-        screen.blit(self.img, (self.rect.x, self.rect.y))
+        self.rect.x = self.pos[0]
+        self.rect.y = self.pos[1]
+
+    def draw(self, screen): 
+        screen.blit(self.img, self.rect)
+        
--- main.py
+++ main.py
@@ -7,25 +7,66 @@ class gameClass:
         self.r = True
         self.size = [700, 700]
         self.screen = pygame.display.set_mode(self.size)
-        self.cubes = [cube.Cube(200, 200, 50, 50)]
+        self.player = cube.Player(400, 400, (200, 0, 0))
+        self.player2 = cube.Player(200, 200, (0, 0, 200))
     def handleEvent(self):
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                self.r = False
+        if pygame.event.get(QUIT):
+            self.r = False
+        for event in pygame.event.get(KEYDOWN):
+            if event.key == K_w:
+                self.player.updateVal(speed=4)
+            if event.key == K_s:
+                self.player.updateVal(speed=-4)
+            if event.key == K_a:
+                self.player.updateVal(angle=2)
+            if event.key == K_d:
+                self.player.updateVal(angle=-2)
+            if event.key == K_UP:
+                self.player2.updateVal(speed=4)
+            if event.key == K_DOWN:
+                self.player2.updateVal(speed=-4)
+            if event.key == K_LEFT:
+                self.player2.updateVal(angle=2)
+            if event.key == K_RIGHT:
+                self.player2.updateVal(angle=-2)
+        for event in pygame.event.get(KEYUP):
+            if event.key == K_w:
+                self.player.updateVal(speed=-4)
+            if event.key == K_s:
+                self.player.updateVal(speed=4)
+            if event.key == K_a:
+                self.player.updateVal(angle=-2)
+            if event.key == K_d:
+                self.player.updateVal(angle=2)
+            if event.key == K_UP:
+                self.player2.updateVal(speed=-4)
+            if event.key == K_DOWN:
+                self.player2.updateVal(speed=4)
+            if event.key == K_LEFT:
+                self.player2.updateVal(angle=-2)
+            if event.key == K_RIGHT:
+                self.player2.updateVal(angle=2)
     def update(self):
         return 0 
     def render(self):
-        for cube in self.cubes:
-            cube.draw(self.screen)
+        self.screen.fill((255, 255, 255))
+        #for cube in self.cubes:
+        #    cube.draw(self.screen)
+        #    cube.update()
+        self.player.update()
+        self.player.draw(self.screen)
+        self.player2.update()
+        self.player2.draw(self.screen)
         pygame.time.Clock().tick(60)
         pygame.display.update()
 
 def main():
     game = gameClass()
+    game.player.updateVal(x=400, y=400)
     while game.r == True:
         game.handleEvent()
         game.update()
         game.render()
-    pygame.exit()
+    pygame.quit()
     return 0 
 sys.exit(main())
